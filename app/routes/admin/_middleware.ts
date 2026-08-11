086200c06589714dd6f559d6891d7ccf7dfc376a
dahliabatikgenturancianjur <dahliabatikgenturancianjur@gmail.com>
2026-08-11 18:56:18 +0700

    Create _middleware.ts

---
+++ app/routes/admin/_middleware.ts
@@ -0,0 +1,21 @@
+import { createRoute } from 'honox/factory'
+import { getCookie } from 'hono/cookie'
+import { verify } from 'hono/jwt'
+
+export default createRoute(async (c, next) => {
+  if (c.req.path === '/admin/login') {
+    return next()
+  }
+
+  const token = getCookie(c, 'admin_token')
+  if (!token) {
+    return c.redirect('/admin/login')
+  }
+
+  try {
+    await verify(token, c.env.JWT_SECRET)
+    return next()
+  } catch (err) {
+    return c.redirect('/admin/login')
+  }
+})
